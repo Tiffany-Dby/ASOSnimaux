9029d7f099359990185ffe69624af2a70bd3b2df
Tiffany-Dby <tiffany.dby@gmail.com>
2024-03-11 22:38:50 +0100

    "removed staff"

--- asosnimaux-bdd/src/routes/init.routes.js
+++ asosnimaux-bdd/src/routes/init.routes.js
@@ -1,6 +1,5 @@
 import initAnimalRoutes from "./animal.routes.js";
 import initArticleRoutes from "./article.routes.js";
-import initStaffRoutes from "./staff.routes.js";
 import initTestimonyRoutes from "./testimony.routes.js";
 import initUserRoutes from "./user.routes.js";
 
@@ -9,7 +8,6 @@ const initRoutes = (app) => {
   initAnimalRoutes(app);
   initUserRoutes(app);
   initTestimonyRoutes(app);
-  initStaffRoutes(app);
   initArticleRoutes(app);
 }
 
