7496e2d50e93478bb7a898a501453706d5cbe0ee
Tiffany-Dby <tiffany.dby@gmail.com>
2024-03-11 22:09:17 +0100

    "comments"

--- asosnimaux-front/src/main.jsx
+++ asosnimaux-front/src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import './styles/index.scss'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+// Styles
+import './styles/index.scss';
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// Components
+import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
